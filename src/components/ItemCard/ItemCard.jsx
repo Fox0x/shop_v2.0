9e1a8b6cebc97a834c8608a27a5e82fda39a0d83
Kirill <foxxx2game@gmail.com>
2022-02-23 14:10:19 +0300

    push on heroku

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,7 +1,17 @@
+import classNames from "classnames";
 import React from "react";
 import css from "./ItemCard.module.css";
 
 export const ItemCard = ({ item, addItemToCart }) => {
+	const [isSelected, setIsSelected] = React.useState(false);
+	const [amount, setAmount] = React.useState(1);
+
+	const itemCardClasses = classNames(css.card, {
+		[css.card_selected]: isSelected,
+	});
+
+	const handleClick = (item) => {};
+
 	return (
 		<div className={css.card}>
 			<img
@@ -14,10 +24,37 @@ export const ItemCard = ({ item, addItemToCart }) => {
 			<h3>{item.title}</h3>
 			<p>{item.description}</p>
 			<div className={css.card__buttons}>
+				<div className={css.card__amount}>
+					<button
+						onClick={() => {
+							amount > 1 && setAmount(amount - 1);
+						}}>
+						-
+					</button>
+					<input
+						type="text"
+						placeholder="0"
+						value={amount}
+						onChange={(e) => {
+							e.target.value = e.target.value.replace(
+								/[^1-9, 0-9]/g,
+								""
+							);
+							setAmount(parseInt(e.target.value) || "");
+						}}
+					/>
+					<button
+						onClick={() => {
+							amount < 100000 && setAmount(amount + 1);
+						}}>
+						+
+					</button>
+				</div>
 				<button
-					// onClick={() => addItemToCart(item)}
-					className="primary">
-					Купить {item.price} ₽
+					onClick={() => handleClick(item)}
+					className="primary"
+					disabled={amount == ""}>
+					Купить {parseInt(item.price.replace(" ", "")) * amount} ₽
 				</button>
 			</div>
 		</div>
