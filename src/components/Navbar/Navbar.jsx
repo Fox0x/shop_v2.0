8e0b59ae301fd2339ef713c62d4a130af3224374
Kirill <foxxx2game@gmail.com>
2022-03-19 14:50:16 +0300

    fix modals

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,21 +8,24 @@ import css from "./Navbar.module.css";
 export const Navbar = ({ searchQuery, setSearchQuery, cartCount }) => {
 	const [isUserInfoOpen, setUserInfoOpen] = React.useState(false);
 	const [isCartOpen, setCartOpen] = React.useState(false);
+	const [isSideMenuOpen, setSideMenuOpen] = React.useState(false);
 	const hideUserInfo = () => setUserInfoOpen(false);
 	const hideCart = () => setCartOpen(false);
+	const hideSideMenu = () => setSideMenuOpen(false);
 	const cartIconClases = classNames(css.cart__count, {
 		[css.count__hidden]: !cartCount > 0,
 	});
 
 	return (
 		<nav className={css.navbar__wrapper}>
-			<Cart isOpen={isCartOpen} hideModal={hideCart}/>
-			<SideMenu/>
+			<Cart isOpen={isCartOpen} hideModal={hideCart} />
+			<SideMenu isOpen={isSideMenuOpen} hideModal={hideSideMenu} />
 			<div className={css.leftSide}>
 				<img
 					className={css.menu__icon}
 					src="/img/svg/menu.svg"
 					alt="menu"
+					onClick={() => setSideMenuOpen(!isSideMenuOpen)}
 				/>
 			</div>
 			<div className={css.centerSide}>
@@ -42,17 +45,19 @@ export const Navbar = ({ searchQuery, setSearchQuery, cartCount }) => {
 					alt="user"
 					onClick={() => setUserInfoOpen(!isUserInfoOpen)}
 				/>
-				<UserInfo isOpen={isUserInfoOpen} hideModal={hideUserInfo}/>
+				<UserInfo isOpen={isUserInfoOpen} hideModal={hideUserInfo} />
 				{/*Shop cart list  */}
-				<div >
-				<img
-					className={css.cart__icon}
-					src="/img/svg/cart.svg"
-					alt="cart"
-					onClick={() => setCartOpen(!isCartOpen)}
-				/>
+				<div>
+					<img
+						className={css.cart__icon}
+						src="/img/svg/cart.svg"
+						alt="cart"
+						onClick={() => setCartOpen(!isCartOpen)}
+					/>
 				</div>
-				<span className={cartIconClases}>{cartCount < 100 ? cartCount : "99+"}</span>
+				<span className={cartIconClases}>
+					{cartCount < 100 ? cartCount : "99+"}
+				</span>
 			</div>
 		</nav>
 	);
--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import classNames from "classnames";
 import { SideMenuItem } from "../SideMenuItem/SideMenuItem";
 import css from "./SideMenu.module.css";
 
 export default function SideMenu({ isOpen, hideModal }) {
+	const sideMenuClasses = classNames(css.sideMenu__wrapper, {
+		[css.sideMenu__hidden]: !isOpen,
+	});
+
 	return (
-		<div className={css.sideMenu__wrapper}>
-			<div className={css.sideMenu__content}>
+		<div className={sideMenuClasses} onClick={() => hideModal()}>
+			<div
+				className={css.sideMenu__content}
+				onClick={(event) => event.stopPropagation()}>
 				<SideMenuItem img="/img/svg/shop.svg" link="/shop">
 					Каталог товаров
 				</SideMenuItem>
